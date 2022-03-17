db9fb4cfb2cef2c8d9fae31ea9e6cb803f510660
Luc <luc.aggett@gmail.com>
2022-03-17 11:27:44 +0100

    Moved Character to DnD_classes
    Character was located in ddpp_classes, which isn't really appropriate since the refactor

--- ddpp_classes.py
+++ ddpp_classes.py
@@ -8,7 +8,6 @@ creating objects for characters and objects
 """
 import os.path
 import platform
-from abc import ABC, abstractmethod
 from os.path import exists
 import sys
 from ddpp import *
@@ -175,91 +174,4 @@ class Weapon:
         crit_range = crit_range[:-1]
         return f"{self.name} {self.attack} {self.damage} {crit_range}"
 
-
-class Character(ABC):  # a 5e Character, can be imported from file
-    """
-    returns a Character object, with most of the stats from a 5e Character sheet.
-    Weopons are created using the Weapon class.
-    """
-
-    def __init__(self):
-        self.name = ""
-        self.stats = {
-            "strength": 0,
-            "dexterity": 0,
-            "constitution": 0,
-            "intelligence": 0,
-            "wisdom": 0,
-            "charisma": 0,
-        }
-        self.speed = 0
-        self.armor_class = 0
-        self.hp = 0
-        self.proficiency = 0
-        self.speed = 0
-        self.armor_class = 0
-        self.proficiencies = []
-        self.saving_throws = []
-        self.skills = []
-        self.weapons = []
-        self.equipment = []
-        self.features = []
-        self.languages = []
-        self.weapon = Weapon(
-            "If you see this something went wrong",
-            "1d100",
-            "1d100",
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        )
-
-    def import_char(self, filepath) -> None:
-        """
-        imports Character stats from a file.
-        """
-        with open(filepath, encoding="utf-8") as character:
-            attrs = vars(self)
-            for line in character:
-                line = line.replace("\n", "")
-                line_tok = line.split(" ")
-                if line_tok[0] in self.stats:
-                    self.stats[line_tok[0]] = line_tok[1]
-                elif line_tok[0] in attrs:
-                    self.__setattr__(line_tok[0], line_tok[1])
-
-    def export_character(self) -> None:
-        """
-        writes the Character file from the object to the file system.
-        """
-        stats = vars(self)
-        with open(f"text/{self.name}.txt", "w", encoding="utf-8") as file:
-            for stat in stats:
-                if stat == "weapon":
-                    file.write(f"Weapon {self.weapon.export()}")
-                else:
-                    file.write(f"{stat} {stats[stat]}\n")
-
-    def create_character(self) -> None:
-        """
-        creates a Character object.
-        """
-
-        self.name = input("Enter Character Name: ")
-        for stat in self.stats:
-            self.stats[stat] = input(f"Enter {stat}: ")
-        self.proficiency = int(input("Enter Proficiency_bonus Bonus: "))
-        self.speed = int(input("Enter Speed: "))
-        self.armor_class = int(input("Enter armor_class: "))
-        self.weapon = Weapon(
-            input("Enter Weapon Name: "),
-            input("Enter Weapon Attack: "),
-            input("Enter Weapon Damage: "),
-            input("Enter Weapon Crit Range: ").split(","),
-        )
-
-    @abstractmethod
-    def attack(self) -> int:
-        """
-        makes an attack!
-        """
-        pass
 
